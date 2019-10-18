1ef54c05e50d34b94be23958696c8789dfd91460
MichaelB <MichaelB@kentico.com>
2019-10-18 14:08:39 -0400

    Added support for coffee pages

---
+++ kontent-sample-app-razorpages/Pages/Coffees/Detail.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Kentico.Kontent.Delivery;
+using KenticoKontentModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kontent_sample_app_razorpages.Pages.Coffees
+{
+    public class DetailModel : PageModel
+    {
+        private IDeliveryClient _deliveryClient;
+
+        public DetailModel(IDeliveryClient deliveryClient)
+        {
+            _deliveryClient = deliveryClient;
+        }
+
+        public Coffee Coffee { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string UrlPattern)
+        {
+            string UrlPatternCodename = "elements." + Coffee.UrlPatternCodename;
+
+            try
+            {
+                var response = await _deliveryClient.GetItemsAsync<Coffee>(
+                new EqualsFilter(UrlPatternCodename, UrlPattern)
+                );
+
+                Coffee = response.Items[0];
+
+                return Page();
+            }
+            catch
+            {
+                return Redirect("/Error");
+            }
+
+        }
+    }
+}
---
+++ kontent-sample-app-razorpages/Pages/Coffees/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kontent_sample_app_razorpages.Pages.Coffees
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
