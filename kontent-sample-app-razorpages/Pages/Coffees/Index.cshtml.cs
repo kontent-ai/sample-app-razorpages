33cd391874c01082c6a8535508cb42e951e7b3ec
MichaelB <MichaelB@kentico.com>
2019-10-18 15:48:55 -0400

    Added coffees listing page.

--- kontent-sample-app-razorpages/Pages/Coffees/Index.cshtml.cs
+++ kontent-sample-app-razorpages/Pages/Coffees/Index.cshtml.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Kentico.Kontent.Delivery;
+using KenticoKontentModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +8,24 @@ namespace kontent_sample_app_razorpages.Pages.Coffees
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
 
+        private IDeliveryClient _deliveryClient;
+
+        public IndexModel(IDeliveryClient deliveryClient)
+        {
+            _deliveryClient = deliveryClient;
+        }
+
+        public DeliveryItemListingResponse<Coffee> Coffee { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Coffee = await _deliveryClient.GetItemsAsync<Coffee>(
+                new EqualsFilter("system.type", "coffee"),
+                new DepthParameter(1)
+            );
+
+            return Page();
         }
     }
 }
