a546ae69d33100f3757f3859423db8373c75522f
Ryan Overton <ryan.overton@outlook.com>
2019-12-12 12:07:32 +0100

    Add About page

---
+++ kontent-sample-app-razorpages/Pages/About/About.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kentico.Kontent.Delivery;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kontent_sample_app_razorpages
+{
+    public class AboutModel : PageModel
+    {
+        private IDeliveryClient _deliveryClient;
+
+        public AboutModel(IDeliveryClient deliveryClient)
+        {
+            _deliveryClient = deliveryClient;
+        }
+
+        public DeliveryItemResponse<KenticoKontentModels.AboutUs> AboutUs { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            AboutUs = await _deliveryClient.GetItemAsync<KenticoKontentModels.AboutUs>("about_us");
+
+            return Page();
+        }
+    }
+}
